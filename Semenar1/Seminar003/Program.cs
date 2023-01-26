45c6844694bde274ac1e23ee2b584e848b674f9f
Igor Khvan <>
2023-01-26 12:37:23 +0300

    Semenar1

---
+++ Semenar1/Seminar003/Program.cs
@@ -0,0 +1,16 @@
+﻿// Проверятель
+Console.Clear();
+
+Console.WriteLine("Вас приветвует Проверятель!!!");
+
+Console.Write("Введите первое число: ");
+int a= Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b= Convert.ToInt32(Console.ReadLine());
+
+if (b*b==a)
+    Console.WriteLine("Да!");
+
+else
+    Console.WriteLine("Нет!");
