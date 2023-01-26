d69525ca9938e7c945f7fa892aa3147ad6345a94
Igor Khvan <>
2023-01-26 12:36:55 +0300

    Lessen1

---
+++ Lessen1/Project001/Program.cs
@@ -0,0 +1,5 @@
+﻿// Вторая программа
+Console.Write("ВВедите Ваше имя: ");
+string username =  Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine (username);
---
+++ Lessen1/Project004/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+ if(username.ToLower () == "елена")
+ {
+    Console.WriteLine("Ура, это же Елена!");
+ }
+ else
+ {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+ }
