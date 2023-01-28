37d4a5edc04dfe8e72a001a057d8ea9d750f62ff
Igor Khvan <>
2023-01-28 23:20:55 +0300

    Project014_Poisk_indexa

---
+++ Lessen2/Project014/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найдём элемент, совпадающий с некоторым значением, который определяет пользователь
+
+Console.Clear();
+
+int[] array = { 1, 12, 31, 4, 15, 16, 17, 17, 18 };
+int n = array.Length;
+
+Console.Write("Введите число: ");
+int find = Convert.ToInt32(Console.ReadLine());
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.Write($"Индекс: {index}");
+        break;
+    }
+        //index = index + 1
+        index++;
+}
