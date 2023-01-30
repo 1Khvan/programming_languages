4d3c5f43e0586987bbaf2920a27e6c1b35e9bc67
Igor Khvan <>
2023-01-30 18:30:49 +0300

    Practice011_task10

---
+++ Practice2/Practice011/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+
+Console.WriteLine("Приветству!");
+
+Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(a);
+int a1 = a % 100;
+int a3 = a % 10;
+
+
+Console.WriteLine((a1-a3)/10);
