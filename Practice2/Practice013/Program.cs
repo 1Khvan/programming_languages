d33ec0b35081bfce956915a79bced9f3fc124169
Igor Khvan <>
2023-01-31 10:53:53 +0300

    Practice013_task13

--- Practice2/Practice013/Program.cs
+++ Practice2/Practice013/Program.cs
@@ -10,17 +10,7 @@ while (n < 1 || n > 7)
     Console.Write("Вы ошиблись!\nВведите число: ");
     n = Convert.ToInt32(Console.ReadLine());
 }
-if (n == 1)
-    Console.WriteLine("Рабочий день: Понедельник - день тяжелый");
-else if (n == 2)
-    Console.WriteLine("Рабочий день: Вторник - самый второй день недели");
-else if (n == 3)
-    Console.WriteLine("Рабочий день: Среда пришла — неделя прошла.");
-else if (n == 4)
-    Console.WriteLine("Рабочий день: Четверг - маленькая Пятница");
-else if (n == 5)
-    Console.WriteLine("Рабочий день: Пятница - IT'S FRIDAY!");
-else if (n == 6)
-    Console.WriteLine("Выходной день: Лучше Пятницы только Суббота!!!");
+if (n < 6)
+    Console.WriteLine("Рабочий день");
 else
-    Console.WriteLine("Выходной: Воскресенье!? Это лучшее, что можно было услышать с утра!");
+    Console.WriteLine("Выходной");
