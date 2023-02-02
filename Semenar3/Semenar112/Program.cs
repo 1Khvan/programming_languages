f77335ed2625532816b0d6edcf911380a1439f50
Igor Khvan <>
2023-02-02 10:55:43 +0300

    Semenar112_task21

---
+++ Semenar3/Semenar112/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+Console.Clear();
+
+Console.Write("Введите координату 1й точки по Х: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату 1й точки по Y: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату 2й точки по Х: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату 2й точки по Y: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+
+double l = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y1 - y2, 2)), 2);
+Console.WriteLine($"Результат: расстояние между точками {l}");
+
+
+
