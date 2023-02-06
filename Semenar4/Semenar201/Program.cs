5998d75e66e8d29bcfcf37a0c48c4e42604beb6e
Igor Khvan <>
2023-02-06 11:05:35 +0300

    Semenar201_task26

---
+++ Semenar4/Semenar201/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе. 456 -> 3 78 -> 2 89126 -> 5
+
+Console.Clear();
+Console.Write("Введите число: ");
+string n = Console.ReadLine();
+int index = n.Length;
+Console.WriteLine (index);
+
