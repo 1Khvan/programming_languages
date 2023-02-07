2d4913c0732e927a246f3202fa33f08d298aac9e
Igor Khvan <hvan.ig@gmail.com>
2023-02-07 15:00:34 +0300

    Semenar202_Array

---
+++ Semenar4/Semenar202/Program.cs
@@ -0,0 +1,15 @@
+﻿// Array
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+int begin = 10, end = 20;
+for (int i = 0; i < array.Length; i++)
+    array[i] = Math.Round(new Random().NextDouble() * (end - begin) + begin, 2);
+
+foreach(double element in array)
+{
+    Console.WriteLine(element);
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
