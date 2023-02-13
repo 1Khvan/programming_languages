2a3c3cb6503e8425aac6e6fc2a0fcceb54ea524e
1Khvan <122914212+1Khvan@users.noreply.github.com>
2023-02-13 11:37:18 +0300

    Delete Semenar2 directory

--- Semenar2/Semenar012/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// 11 Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
-
-Console.Clear();
-int n = new Random().Next(100, 1000); // [10, 1000]
-Console.WriteLine(n);
-int n1 = n / 100;
-int n3 = n % 10;
-
-Console.WriteLine(n1*10+n3);
