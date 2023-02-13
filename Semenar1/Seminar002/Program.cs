591c216841a1544e1f6eda549bbd4693fc8c0a73
1Khvan <122914212+1Khvan@users.noreply.github.com>
2023-02-13 11:37:07 +0300

    Delete Semenar1 directory

--- Semenar1/Seminar002/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// Возводитель в квадрат
-Console.Clear();
-
-Console.WriteLine("Вас приветвует Возводитель в квадрат!!!");
-Console.Write("Введите число: ");
-int n= Convert.ToInt32(Console.ReadLine());
-Console.Write($"Рузультат: {n*n}");
