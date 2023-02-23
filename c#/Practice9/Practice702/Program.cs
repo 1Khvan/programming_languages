88afed4923ca87b61ab0663fff11761bdfcacf1c
Igor Khvan <>
2023-02-23 11:36:26 +0300

    .\Practice702_task66

---
+++ c#/Practice9/Practice702/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int sumCifr(int n, int m)
+{
+    if (n == m)
+        return n;
+    return sumCifr(n,m - 1) +  m;
+}
+
+
+Console.Clear();
+Console.Write("Введите число1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число2: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sumCifr(n,m));
