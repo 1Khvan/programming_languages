33f6cf45c6a85af40b0180bf3f65aeb5c788b76d
Igor Khvan <>
2023-04-14 10:32:22 +0300

    .\Seminar3

---
+++ Python/Seminar3/Task1.py
@@ -0,0 +1,9 @@
+# Дан список чисел. Определите, сколько в нем встречается различных чисел.
+
+list_numbers = [1,1,2,3,4,4,4,5,6,7,7]
+list_unic_numbers=[]
+list_unic_numbers=set(list_numbers)
+len(list_unic_numbers)
+print (list_numbers)
+print(len(list_unic_numbers))
+
